6f3bbfed58501dd9b56ab786708a6267ef9b6ea2
镜雪YukiNA <1730731981@qq.com>
2026-03-28 10:24:33 +0800

    删Proficiency

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -2,7 +2,6 @@ import React, {useEffect, useState} from "react";
 import Header from "../components/header/Header";
 import Greeting from "./greeting/Greeting";
 import Skills from "./skills/Skills";
-import StackProgress from "./skillProgress/skillProgress";
 import WorkExperience from "./workExperience/WorkExperience";
 import Blogs from "./blogs/Blogs";
 import Footer from "../components/footer/Footer";
@@ -50,7 +49,6 @@ const Main = () => {
             <Header />
             <Greeting />
             <Skills />
-            <StackProgress />
             <Education />
             <WorkExperience />
             <Blogs />
